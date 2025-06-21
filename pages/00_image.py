81b7c9ac965eea0eb87034efa9edf3a98348c3fa
seojiyo <gomin2062@gmail.com>
2025-06-21 13:09:24 +0900

    Create 00_image.py

---
+++ pages/00_image.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWcwMjlkN2oxMTNpNzhvY2NwdGZ0djd3cmVmNDY0a3k1aGk3cW9odCZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/TqV8LqhtM1MrxA1ih9/giphy.gif")
